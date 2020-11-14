3fb967b0aea6912dc8eb8e35a2b2185c06dde7ab
Rudywasnthere <rudybcchs@gmail.com>
2020-11-14 03:31:45 +0000

    I can't get the timer to work how it is supposd to so :/, but that alright I guess

--- main.py
+++ main.py
@@ -1,78 +1,55 @@
 ##Rudy Garcia
 import math, sys
 import time, sys
-
-
-def correct_input(x):
-    is_integer = False
-    while is_integer is False:
-        try:
-          x = int(x)
-          is_integer = True
-        except ValueError:
-          x = input("I need a valid number, please:\t") 
-          is_integer = False
-    return x
-
-def initials(x):
-    sqrt_num = math.sqrt(x)
-    divisor = math.floor(sqrt_num)
-    end_root= divisor
-    divisor_even= divisor%2
-    x_even= x%2
-    if x_even==0:
-        count_by =1
-    elif x_even ==1 and divisor_even==0:
-        count_by =2
-        divisor -= 1
-    elif x_even ==1 and divisor_even ==1:
-      count_by=2
-    return count_by, divisor, end_root
-
-def factor_finder(number, count_by, divisor):
-    factor_list = ""
-    count_factors = 0
-    t_1= time.process_time()
-    while divisor>=1: 
-      upper_factor = number/divisor
-      if upper_factor.is_integer() is True:
-        upper_factor = int(upper_factor)
-        factor_list += f"({divisor},{upper_factor})\n"
-        count_factors += 1
-      else:
-        factor_list += ""
-      divisor -= count_by
-    t_2= time.process_time()
-    total_time= t_2 - t_1
-    if count_factors == 1:
-      verb, factor_ending, third_part= "is", "", "it is"
-    else:
-      verb, factor_ending, third_part= "are", "s", "they are"
-    return factor_list, count_factors, total_time, verb, factor_ending, third_part
-
-def final_outputs(x, verb, count_factors, factor_ending, third_part, factors, total_time, end_root, speed):
-  final_output= ""
-  if count_factors== 1:
-    final_output += f"\n{x} is a prime number!\n"
-  final_output += F"There {verb} {count_factors} factor{factor_ending}! Here {third_part}:\n{factors}\nIt took {total_time} seconds to process {end_root} numbers!\nThats {speed} numbers per second!"
-  return final_output
-
-print("Hello, this is a program for finding the factors for any postive number you input")
-restart= ""
+restart = ""
 count = 0
-while restart =="":
-    if count == 0:
-      x = input("To start, what is the number you want to factor:\t")
-    if count != 0: 
-      x= input("What number do you want to factor:\t")
-    x= correct_input(x)
-    count_by, divisor, end_root= initials(x)
-    factors, count_factors, total_time, verb, factor_ending, third_part = factor_finder(x, count_by, divisor)
-    speed_1= float(end_root/total_time)
-    speed = round(speed_1,10)
-    print(f"{final_outputs(x, verb, count_factors, factor_ending, third_part, factors, total_time, end_root, speed)}")
-    time.sleep(2)  
-    restart= input("Hit enter to restart or enter any key to exit:\t")
-    count += 1
-  time.sleep(3)
+print("Hello, this is a program for finding the factors for any postive numput you input")
+
+def factor_finder(number):
+  factor_list = ""
+  count_factors = 0
+  is_integer = False
+  while is_integer is False:
+    try:
+      number = int(number)
+      is_integer = True
+    except ValueError:
+      number = input("I need a valid number, please:\t") 
+      is_integer = False
+  sqrt_num = math.sqrt(number)
+  divisor = math.floor(sqrt_num)
+  end_root= divisor
+  while divisor>0: 
+    upper_factor = number/divisor
+    if upper_factor.is_integer() is True:
+      upper_factor = int(upper_factor)
+      factor_list += f"({divisor},{upper_factor})\n"
+      count_factors += 1
+    else:
+      factor_list += ""
+    divisor -= 1
+  return factor_list, end_root, count_factors
+
+while restart == "":
+  if count == 0:
+    x = input("To start, what is the number you want to factor:\t")
+  if count != 0: 
+    x= input("What number do you want to factor:\t")
+  t_1 = time.perf_counter()
+  factors, end_root, count_factors = factor_finder(x)
+  t_2 = time.perf_counter()
+  process_time= t_2 - t_1
+  speed= end_root/process_time
+  if count_factors ==1:
+    verb= 'is'
+    factor_ending= ""
+    third_part = "it is"
+    print(f"\n{x} is a prime number!\n")
+  elif count_factors !=1:
+    verb= "are"
+    factor_ending= "s"
+    third_part = "they are"
+  print(f"There {verb} {count_factors} factor{factor_ending}! Here {third_part}:\n{factors}\nIt took {process_time} seconds to process {end_root} numbers!\nThats {speed} numbers per second!")
+  restart= input("Hit enter to restart, or enter any key to quit ")
+  count +=1 
 
